68194836f58a501a06d065c8b988f4299ff9259f
lararjason <jason.norda@lbs.se>
2022-09-15 08:47:31 +0200

    Lektion2

--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -2,11 +2,20 @@ function build(){
     var images = ['assets/img/1.jpg','assets/img/2.jpg','assets/img/3.png','assets/img/4.jpg','assets/img/5.jpg'];
     jQuery.each(images, function(i, val){
         $('#photocontainer').append('<img src="' + val + '">');
+        $('#nav').append('<a href="#" data-img="' + i + '">' + (i + 1) + '</a>');
     });
     $('#photocontainer img:eq(0)').addClass('active');
     $('#photocontainer img').click(function(){
         nextImg();
     });
+    $('#nav a').click(function(){
+        findImg($(this).data('img'));
+    });
+}
+
+function findImg(which){
+    $('#photocontainer img.active').removeClass('active');
+    $('#photocontainer img').eq(which).addClass('active');
 }
 
 function nextImg(){
@@ -24,5 +33,4 @@ $(document).ready(function(){
 });
 
 
-$('img').atrr('src', 'new.png');
 
